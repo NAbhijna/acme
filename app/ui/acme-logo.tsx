8622374b704e9e8eda5d50dfe29735d0079d3cd6
NAbhijna <abhijnanalka@gmail.com>
2025-07-07 00:48:54 +0530

    fix error

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,6 +1,5 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/font';
-import {Image} from 'next/image';
 
 export default function AcmeLogo() {
   return (
